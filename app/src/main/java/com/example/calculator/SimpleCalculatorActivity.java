802bd43f12fe06326f0c2b0546a29044a594e20b
michaaeu <mik3wwl@gmail.com>
2020-06-24 03:23:08 +0200

    Switching activity: SimpleCalculatorActivity

--- app/src/main/java/com/example/calculator/SimpleCalculatorActivity.java
+++ app/src/main/java/com/example/calculator/SimpleCalculatorActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SimpleCalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SimpleCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_calculator);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
