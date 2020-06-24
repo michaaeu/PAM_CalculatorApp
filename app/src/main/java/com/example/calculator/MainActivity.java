d6611cff78b4c41e8a5d1a0871b1c102622f27f0
michaaeu <mik3wwl@gmail.com>
2020-06-24 22:44:35 +0200

    Some buttons onClick actions

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,20 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
+    // BUTTONS //
     public void btnClick_simple(View view) {
         startActivity(new Intent(MainActivity.this, SimpleCalculatorActivity.class));
     }
 
     public void btnClick_about(View view) {
+        Snackbar.make(findViewById(R.id.mainLayout), "App created by michaaeu", Snackbar.LENGTH_LONG).show();
     }
 
     public void btnClick_exit(View view) {
@@ -27,5 +30,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick_advanced(View view) {
+        Snackbar.make(findViewById(R.id.mainLayout), "Buy full version to use Advanced Mode", Snackbar.LENGTH_LONG).show();
     }
+
 }
